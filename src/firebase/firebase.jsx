fec5756a7a50f978996dd1ed37fc021eafb61794
Adriano Nascimento <adrianojunior.dev97@gmail.com>
2022-09-28 14:59:56 -0300

    Settings

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -1,4 +1,15 @@
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDPSA1M2zVARBcIjLxlKVSVRzlpvxPayo4",
+  authDomain: "partyou-ae814.firebaseapp.com",
+  projectId: "partyou-ae814",
+  storageBucket: "partyou-ae814.appspot.com",
+  messagingSenderId: "499481635745",
+  appId: "1:499481635745:web:988db3c16e5b1e42b239e0"
+};
+
+const app = initializeApp(firebaseConfig);
 
 
 export const fb = {
